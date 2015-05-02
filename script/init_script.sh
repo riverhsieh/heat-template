0b12bf974446589c4af98eb8649bdc6eb4a993a4
River_Hsieh <river_hsieh@compal.com>
2015-05-02 12:50:26 +0800

    update python virtual env

--- script/init_script.sh
+++ script/init_script.sh
@@ -11,7 +11,7 @@ EOF
 
 apt-get update
 
-apt-get install -y apt-transport-https git socat connect-proxy
+apt-get install -y apt-transport-https git socat connect-proxy python2.7-dev python-virtualenv 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
 
@@ -20,10 +20,6 @@ apt-get install -y lxc-docker
 sed -i "s|127.0.0.1:3128|$ip_port|" /etc/default/docker
 sed -i 's/^#export http/export http/' /etc/default/docker
 service docker restart
-
-apt-get install -y python2.7-dev
-apt-get install -y python-pip
-pip --proxy $http_proxy install python-heatclient
 
 cat >/home/ubuntu/openrc <<EOF
 #!/bin/sh
@@ -35,34 +31,39 @@ export OS_PASSWORD='admin'
 export OS_AUTH_URL='http://192.168.0.3:5000/v2.0/'
 export OS_AUTH_STRATEGY='keystone'
 EOF
-
-chown ubuntu:ubuntu /home/ubuntu/openrc
 chmod 644 /home/ubuntu/openrc
 
 cat >/home/ubuntu/nsenter.sh <<EOF
 #!/bin/sh
-cd /tmp
 curl https://www.kernel.org/pub/linux/utils/util-linux/v2.24/util-linux-2.24.tar.gz | tar -zxf-
 cd util-linux-2.24
 ./configure --without-ncurses
 make nsenter
-cp nsenter /usr/local/bin
-EOF
-
-chown ubuntu:ubuntu /home/ubuntu/nsenter.sh
-chmod 755 /home/ubuntu/nsenter.sh
-. /home/ubuntu/nsenter.sh
-
-cd /home/ubuntu
+sudo cp nsenter /usr/local/bin
+cd ..
 git config --global http.proxy $http_proxy
 git config --global https.proxy $https_proxy
 git clone https://github.com/jpetazzo/pipework.git
+EOF
+chmod 755 /home/ubuntu/nsenter.sh
 
 cat >/home/ubuntu/docker_enter <<EOF
 #!/bin/sh
 PID=\$(docker inspect --format '{{.State.Pid}}' my_container_id)
 nsenter --target \$PID --mount --uts --ipc --net --pid
 EOF
-
-chown ubuntu:ubuntu /home/ubuntu/docker_enter
 chmod 644 /home/ubuntu/docker_enter
+
+cat >/home/ubuntu/python_venv <<EOF
+#!/bin/sh
+virtualenv venv
+source venv/bin/activate
+pip --proxy $http_proxy install python-heatclient
+pip --proxy $http_proxy install python-novaclient
+EOF
+chmod 644 /home/ubuntu/python_venv
+
+chown -R ubuntu:ubuntu /home/ubuntu/
+
+echo export http_proxy=$http_proxy >> /etc/profile
+echo export https_proxy=$https_proxy >> /etc/profile
